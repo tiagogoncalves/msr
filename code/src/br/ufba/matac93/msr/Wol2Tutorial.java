b16f257f0b1d099b932fef3c68d2a4334d02a941
Tiago Gonçalves <tiagogsba@gmail.com>
2014-02-08 18:54:03 -0300

    Adicionando presequel modificado

--- code/src/br/ufba/matac93/msr/Wol2Tutorial.java
+++ code/src/br/ufba/matac93/msr/Wol2Tutorial.java
@@ -20,23 +20,6 @@ import uk.ac.manchester.cs.owlapi.dlsyntax.DLSyntaxObjectRenderer;
 
 import com.clarkparsia.pellet.owlapiv3.PelletReasonerFactory;
 
-import com.clarkparsia.owlapi.explanation.DefaultExplanationGenerator; 
-import com.clarkparsia.owlapi.explanation.util.SilentExplanationProgressMonitor; 
-import com.clarkparsia.pellet.owlapiv3.PelletReasonerFactory; 
-import org.semanticweb.owlapi.apibinding.OWLManager; 
-import org.semanticweb.owlapi.io.OWLObjectRenderer; 
-import org.semanticweb.owlapi.model.*; 
-import org.semanticweb.owlapi.reasoner.OWLReasoner; 
-import org.semanticweb.owlapi.reasoner.OWLReasonerFactory; 
-import org.semanticweb.owlapi.reasoner.SimpleConfiguration; 
-import org.semanticweb.owlapi.vocab.OWLRDFVocabulary; 
-import org.semanticweb.owlapi.vocab.PrefixOWLOntologyFormat; 
-import uk.ac.manchester.cs.bhig.util.Tree; 
-import uk.ac.manchester.cs.owl.explanation.ordering.ExplanationOrderer; 
-import uk.ac.manchester.cs.owl.explanation.ordering.ExplanationOrdererImpl; 
-import uk.ac.manchester.cs.owl.explanation.ordering.ExplanationTree; 
-import uk.ac.manchester.cs.owlapi.dlsyntax.DLSyntaxObjectRenderer; 
-
 public class Wol2Tutorial {
 
 	
@@ -68,6 +51,7 @@ public class Wol2Tutorial {
         OWLDataFactory factory2 = manager2.getOWLDataFactory(); 
         PrefixOWLOntologyFormat pm2 = (PrefixOWLOntologyFormat) manager2.getOntologyFormat(ontology2);
 
+        
         String ns = "http://www.semanticweb.org/administrator/ontologies/2014/1/untitled-ontology-2";
 
         pm2.setDefaultPrefix(ns + "#"); 
